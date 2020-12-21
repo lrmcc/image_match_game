4c74c6bdbdc9937ae734d7c697716db719ae64ea
lrmcc <lmcconnelldev@gmail.com>
2020-12-20 21:01:55 -0700

    game reset functional

--- memory_match.js
+++ memory_match.js
@@ -5,52 +5,10 @@ let gridAssignments;
 let gameRunning = 0;
 let score = 0;
 
-function setGrid(){
-    gridColors = ["red", "yellow", "green", "blue", "orange","purple", "pink", "brown"];
-    gridNumbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16];
-    gridAssignments = {"grid-item-1": "black", "grid-item-2": "black", 
-    "grid-item-3": "black", "grid-item-4": "black", "grid-item-5": "black",
-    "grid-item-6": "black", "grid-item-7": "black", "grid-item-8": "black", 
-    "grid-item-9": "black", "grid-item-10": "black", "grid-item-11": "black",
-    "grid-item-12": "black", "grid-item-13": "black", "grid-item-14": "black",
-    "grid-item-15": "black","grid-item-16": "black"};
-}
-
-function getRandomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
-}
-
-function assignColor(num1, num2, clr){
-    num1ID = "grid-item-" + num1;
-    console.log("num1ID: " + num1ID);
-    num2ID = "grid-item-" + num2;
-    console.log("num2ID: " + num2ID);
-    console.log("clr: " + clr);
-    gridAssignments[num1ID] = clr;
-    gridAssignments[num2ID] = clr;
-    console.log(gridAssignments[num1ID]);
-    console.log(gridAssignments[num2ID]);
-
-}
-
-function setBlack(){
-    let card1 = document.getElementById(cardsShowing[0]);
-    let card2 = document.getElementById(cardsShowing[1]);
-    card1.style.backgroundColor = "black";
-    card2.style.backgroundColor ="black";
-    cardsShowing = [];
-}
-
-function removeNumber(num){
-    let index = gridNumbers.indexOf(num);
-    if (index > -1) {
-        gridNumbers.splice(index, 1);
-    }
-}
-
 function startGame(){
+    resetGrid();
+    resetGame();
     gameRunning = 1;
-    setGrid();
     for(let i = 0; i < 8; i++){
         console.log("----------------");
         let num1 = gridNumbers[Math.floor(Math.random() * gridNumbers.length)];
@@ -65,6 +23,70 @@ function startGame(){
     }
     console.log(gridAssignments)
     runGame();
+}
+
+function resetGrid(){
+    gridColors = ["red", "yellow", "green", "blue", "orange","purple", "pink", "brown"];
+    gridNumbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16];
+    gridAssignments = {"grid-item-1": "black", "grid-item-2": "black", 
+    "grid-item-3": "black", "grid-item-4": "black", "grid-item-5": "black",
+    "grid-item-6": "black", "grid-item-7": "black", "grid-item-8": "black", 
+    "grid-item-9": "black", "grid-item-10": "black", "grid-item-11": "black",
+    "grid-item-12": "black", "grid-item-13": "black", "grid-item-14": "black",
+    "grid-item-15": "black","grid-item-16": "black"};
+}
+
+function resetGame(){
+    console.log("resetting game");
+    let scoreDisplay = document.getElementById("game-score");
+    scoreDisplay.innerText = "Score: 0";
+    resetCards();
+}
+
+function resetCards(){
+    for (const [key, value] of Object.entries(gridAssignments)) {
+        console.log("key: " + key);
+        console.log("value: " + value);
+        let card = document.getElementById(key);
+        card.style.backgroundColor = value;
+        card.setAttribute("onclick", "showColor(this)");
+        //card.onclick = "showColor(this)";  .removeAttribute("class");
+
+    }
+}
+
+function removeNumber(num){
+    let index = gridNumbers.indexOf(num);
+    if (index > -1) {
+        gridNumbers.splice(index, 1);
+    }
+}
+
+function assignColor(num1, num2, clr){
+    num1ID = "grid-item-" + num1;
+    console.log("num1ID: " + num1ID);
+    num2ID = "grid-item-" + num2;
+    console.log("num2ID: " + num2ID);
+    console.log("clr: " + clr);
+    gridAssignments[num1ID] = clr;
+    gridAssignments[num2ID] = clr;
+    console.log(gridAssignments[num1ID]);
+    console.log(gridAssignments[num2ID]);
+}
+
+function runGame(){
+    console.log("game running");
+    if (cardsShowing.length == 2){
+        checkCards();
+    }
+}
+
+function setBlack(){
+    let card1 = document.getElementById(cardsShowing[0]);
+    let card2 = document.getElementById(cardsShowing[1]);
+    card1.style.backgroundColor = "black";
+    card2.style.backgroundColor ="black";
+    cardsShowing = [];
 }
 
 function showColor(item){
@@ -94,8 +116,8 @@ function checkCards() {
     console.log(card2Color);
     if (card1Color == card2Color){
         console.log("Match!!");
-        card1.onclick = null;
-        card2.onclick = null;
+        card1.removeAttribute("onclick");
+        card2.removeAttribute("onclick");
         card1.style.opacity = 0.5;
         card2.style.opacity = 0.5;
         updateScore();
@@ -111,16 +133,12 @@ function updateScore(){
     console.log("score: " + score);
     let scoreDisplay = document.getElementById("game-score");
     scoreDisplay.innerText = "Score: " + score;
-    if (score == 8) {
-        alert("You WIN!!!");
-        resetGame();
-    }
-    
+    checkWin();
 }
 
-function resetGame(){
-    console.log("resetting game");
-    gameRunning = 0;
-    let scoreDisplay = document.getElementById("game-score");
-    scoreDisplay.innerText = "Score: 0";
+function checkWin(){
+    if (score == 8) {
+        gameRunning = 0;
+        alert("You WIN!!!");
+    }
 }
